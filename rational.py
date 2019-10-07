c9e1a7d912ef15b069f952b0ba67e3352fbae494
SHIRAI Riko <21716051@jwu.ac.jp>
2019-10-07 16:11:20 +0900

    class Q3

--- rational.py
+++ rational.py
@@ -2,8 +2,10 @@ class Q(object):
     def __init__(self, a, b):
         self.a = a
         self.b = b
+    #def __repr__(self):
+        #return 'hoge'
     def __repr__(self):
-        return 'hoge'
+        return f'{self.a}/{self.b}'
     
 
 q = Q(1,2)
