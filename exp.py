aba36623d366ffdebe35dd7eb1e9aa6564d77083
SHIRAI Riko <21716051@jwu.ac.jp>
2019-10-14 16:37:37 +0900

    Add3

--- exp.py
+++ exp.py
@@ -19,23 +19,32 @@ assert isinstance(v, Expr)
 assert isinstance(v, Val)
 assert not isinstance(v, int)
 
+def toExpr(x):
+    if not isinstance(a, Expr):
+        a = Val(a)
+    return a
+
 class Add(Expr):
     __slots__=['left', 'right']
     def __init__(self, a, b):
+        if not isinstance(a, Expr):
+            a = Val(a)
+        if not isinstance(b, Expr):
+            b = Val(b)
         self.left = a
         self.right = b
     def eval(self):
         return self.left.eval() + self.right.eval()
 
+e = Add(1,Add(1,2))
+print(e.eval())
+assert e.eval() == 4
 e = Add(Val(1), Val(2))
 assert e.eval() == 3
-
+print(e.eval())
 e = Add(1,2)
 assert e.eval() == 3
-
+print(e.eval())
 e = Add(Val(1),Add(Val(2),Val(3)))
 assert e.eval() == 6
-
-print()
-print()
-print()
+print(e.eval())
