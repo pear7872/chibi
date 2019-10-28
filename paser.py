cd4c32bc03ff6a2b051fda7f004a2409ed3bbd26
SHIRAI Riko <21716051@jwu.ac.jp>
2019-10-28 16:19:38 +0900

    output Add

--- paser.py
+++ paser.py
@@ -3,7 +3,7 @@ from exp import Val, Add
 def parse(s: str):
     num = int(s)
     return Val(num)
-
+'''
 e = parse("123")
 print(e)
 
@@ -14,5 +14,15 @@ print('pos', pos)
 s1 = s[0:pos]
 s2 = s[pos+1:]
 print(s, s1, s2)
+'''
+def parse(s: str):
+    pos = s.find('+')
+    if pos == -1:
+        return Val(int(s))
+    else:
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
 
-
+e = ("123+456+789")
+print(e)
