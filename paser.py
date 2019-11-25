7273ee3a768974d10d24fc0b4bf9c99e024e6c9f
SHIRAI Riko <21716051@jwu.ac.jp>
2019-11-25 15:44:15 +0900

    parser

--- paser.py
+++ paser.py
@@ -1,4 +1,4 @@
-from exp import Val, Add
+from exp import Val, Add, Sub, Mul, Div
 
 def parse(s: str):
     num = int(s)
@@ -14,15 +14,26 @@ print('pos', pos)
 s1 = s[0:pos]
 s2 = s[pos+1:]
 print(s, s1, s2)
-'''
+
 def parse(s: str):
-    pos = s.find('+')
-    if pos == -1:
-        return Val(int(s))
-    else:
+    if s.find('+') > 0:
+        pos = s.find('+')
         s1 = s[0:pos]
         s2 = s[pos+1:]
-        return Add(parse(s1), parse(s2))
+        return Add(parse(s1),parse(s2))
+    if s.find('*') > 0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+'''
+def parse(s: str):
+    if s.find('-') > 0:
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1), parse(s2))
+    return Val(int(s))
 
-e = ("123+456")
-print(e)
+e = parse("1-2-3")
+print(e, e.eval())   # -4
